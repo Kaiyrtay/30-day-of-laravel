4ad2f512bb91e47a47cb52183e49326de167d681
kaiyrtay <kaiyrtaygabbassov@gmail.com>
2024-08-06 14:36:49 +0300

    Day 3,4.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
-    <h1>Contact, page.</h1>
+    <x-slot:heading>Contact, page.</x-slot:heading>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, eligendi doloremque voluptatem similique quia saepe ex blanditiis odit facilis nostrum voluptatum debitis corrupti?</p>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
-<x-layout>
+<x-layout> 
+    <x-slot:heading>
+        Home page
+    </x-slot:heading>
     <h1>Home, page.</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, eligendi doloremque voluptatem similique quia saepe ex blanditiis odit facilis nostrum voluptatum debitis corrupti?</p>
 </x-layout>
