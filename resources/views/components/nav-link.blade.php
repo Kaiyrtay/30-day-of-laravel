2fefd106fef4a5326e601f407dc2fafebcca7b43
kaiyrtay <kaiyrtaygabbassov@gmail.com>
2024-08-06 15:51:46 +0300

    Day 5 homework.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,18 @@
-@props(['active' => false])
-
+@props(['active' => false, 'type' => 'a'])
+<?php if($type === 'a'):?>
 <a class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
     aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
     {{ $slot }}
 </a>
+<?php else:?>
+<button
+    class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+    {{ $slot }}
+</button>
+<?php endif?>
+
+{{-- <{{type}} class=" {{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
+    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>
+    {{ $slot }}
+</{{type}} > --}}
