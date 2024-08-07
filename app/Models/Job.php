f5b21c4aea1df77773a594683b1df314bbf287f9
kaiyrtay <kaiyrtaygabbassov@gmail.com>
2024-08-07 12:54:00 +0300

    Day 11 was quite intersting, and boring.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,15 +4,23 @@ namespace App\Models;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Job extends Model
 {
     use HasFactory;
     protected $table = 'job_listing';
-    protected $fillable = ['title','salary'];
+    protected $fillable = ['title', 'salary'];
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class, foreignPivotKey:'job_listing_id');
+    }
     // public static function all(): array
     // {
     //     // return [
-
     //     //     [
     //     //         'id' => 1,
     //     //         'title' => 'Director',
@@ -28,19 +36,14 @@ class Job extends Model
     //     //         'title' => 'Teacher',
     //     //         'salary' => '$30,000'
     //     //     ]
-
     //     // ];
-
     // }
-
     // public static function find(int $id): array
     // {
     //     $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
     //     if (!$job) {
     //         abort(404);
     //     }
-
     //     return $job;
     // }
 }
