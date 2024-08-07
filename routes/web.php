4b13157541c045769ec549fd4f52ec5c9d7a2731
kaiyrtay <kaiyrtaygabbassov@gmail.com>
2024-08-07 08:22:44 +0300

    Day 7.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,51 @@
 <?php
 
-use Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
+// Option 1
+// $jobs = [
+//     [
+//         'id' => 1,
+//         'title' => 'Director',
+//         'salary' => '$50,000'
+//     ],
+//     [
+//         'id' => 2,
+//         'title' => 'Programmer',
+//         'salary' => '$10,000'
+//     ],
+//     [
+//         'id' => 3,
+//         'title' => 'Teacher',
+//         'salary' => '$30,000'
+//     ]
+// ];
+// Option 2
+// class Job
+// {
+//     public static function all(): array
+//     {
+//         return [
+//             [
+//                 'id' => 1,
+//                 'title' => 'Director',
+//                 'salary' => '$50,000'
+//             ],
+//             [
+//                 'id' => 2,
+//                 'title' => 'Programmer',
+//                 'salary' => '$10,000'
+//             ],
+//             [
+//                 'id' => 3,
+//                 'title' => 'Teacher',
+//                 'salary' => '$30,000'
+//             ]
+//         ];
+//     }
+// }
 
 Route::get('/', function () {
     return view('home');
@@ -16,49 +60,18 @@ Route::get('/contact', function () {
 });
 
 // return view and array or data
-Route::get('/jobs', function () {
+Route::get('/jobs', function () { //use ($jobs) { // option 1
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$30,000'
-            ]
-        ]
+        'jobs' => Job::all() //$jobs // option 1
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$30,000'
-        ]
-    ];
+Route::get('/jobs/{id}', function ($id) { //use ($jobs) { // option 1
     // Illuminate\Support\Arr::first($jobs, function($job) use($id){
     //     return $job['id'] == $id;
     // });
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-    return view('job',['job'=>$job]);
+    // $job = Arr::first($jobs, fn($job) => $job['id'] == $id); // Option 1
+    // $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id); //Option 2
+    $job = Job::find($id);
+    return view('job', ['job' => $job]);
 });
